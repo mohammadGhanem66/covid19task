f9ba9204bbffda1b6c2a5386e671f4b02101d2c0
Mohammad Ghanem <mohammad2Ghanem@hotmail.com>
2023-01-01 22:13:41 +0200

    Add required changes.

--- covidtask/covidtask/serializers.py
+++ covidtask/covidtask/serializers.py
@@ -1,26 +1,25 @@
 from rest_framework import serializers
 from .models import Country
-from django.contrib.auth.models import User
+
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = "__all__"
 
-class SubscribtionSerializer(serializers.Serializer):
-    countries = serializers.ListField(child =serializers.CharField())
-    userId = serializers.IntegerField()
-    def create(self,validated_data):
-        user = User.objects.get(id=validated_data['userId'])
-        countries = Country.objects.filter(name__in=validated_data['countries']).all()
-        for country in countries:
-            user.subscriptions.add(country)
-        user.save()
-        return user;
 
-
-class UserSerializer(serializers.ModelSerializer):
-    country = CountrySerializer(many=True,source="subscriptions")
+class SubscribtionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id','username','country']
+        model = Country.subscription.through
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['country'] = instance.country.name
+        data['user'] = instance.user.username
+        return data
+
+
+class CountryViewSerializer(serializers.Serializer):
+    status = serializers.CharField()
+    limit = serializers.IntegerField(default=3)
--- covidtask/covidtask/views.py
+++ covidtask/covidtask/views.py
@@ -1,53 +1,82 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes,action
-from .models import Country
-from .serializers import CountrySerializer,SubscribtionSerializer
-from rest_framework.generics import ListAPIView,CreateAPIView
-from rest_framework.viewsets import ViewSetMixin,ViewSet
-from rest_framework import views,status
+from .models import Country, CountryDailyCases
+from .serializers import CountrySerializer, SubscribtionSerializer, CountryViewSerializer
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework import views, status
 from django.db.models import Count
-from django.contrib.auth.models import User
 
-class CountryViewSet(ViewSetMixin,ListAPIView):
+
+class CountryPercentageView(ListAPIView):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
-    @action(detail=False,methods=('GET',),url_path="death-percentage")
-    def get_percentage(self,request,*args,**kwargs):
-        if 'country' not in request.query_params:
-            return Response({'message': 'Missing parameters (country)', 'error': True, 'code': 400}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, country, *args, **kwargs):
+
+        country = Country.objects.filter(name=country).first()
+        if not country:
+            return Response({'message': 'Country not found', 'error': False, 'code': 400},
+                            status=status.HTTP_400_BAD_REQUEST)
         try:
-            country = Country.objects.filter(name=request.query_params['country']).first()
-            countrySerializer = CountrySerializer(country)
-            deathCount = countrySerializer.data['death']
-            confirmedCount = countrySerializer.data['confirmed']
-            percentage = deathCount / confirmedCount
-            responseData = {"death": deathCount, "confirmed": confirmedCount, "percentage": percentage}
-            return Response(responseData)
-        except:
-            return Response()
+            country_history = country.country_daily_cases.latest('created_at')
+        except CountryDailyCases.DoesNotExist:
+            country_history = None
 
-    @action(detail=False, methods=('GET',), url_path="top")
-    def get(self, request, *args, **kwargs):
-        if 'status' not in request.query_params:
-            return Response({'message': 'Missing parameters (status)', 'error': True, 'code': 400}, status=status.HTTP_400_BAD_REQUEST)
-        stauts = request.query_params['status']
-        if 'limit' in request.query_params:
-            limit = int(request.query_params['limit'])
+        if country_history:
+            death_count = country_history.death_agg
+            confirmed_count = country_history.confirmed_agg
+            if not confirmed_count:
+                return Response({'message': 'Confirmed cant be zero', 'error': False, 'code': 500},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            percentage = death_count / confirmed_count
+            responseData = {"death": death_count, "confirmed": confirmed_count, "percentage": percentage}
         else:
-            limit = 3
+            responseData = {"death": 0, "confirmed": 0, "percentage": 0}
+        return Response(responseData)
 
-        countries = Country.objects.annotate(subscriptions_count=Count('subscription')).filter(subscriptions_count__gt =0 )\
-            .all().order_by("-"+stauts)[:limit]
 
-        countrySerializer = CountrySerializer(countries,many=True)
+class TopCountryView(ListAPIView):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        country_view_serializer = CountryViewSerializer(data=request.query_params)
+        country_view_serializer.is_valid(raise_exception=True)
+        data = country_view_serializer.validated_data
+        status_agg = data['status'] + "_agg"
+        subscribed_countries = Country.objects.annotate(subscriptions_count=Count('subscription')) \
+            .filter(subscriptions_count__gt=0).all()
+        countries_history = [self.get_latest_country_daily_cases(country) for country in subscribed_countries]
+        countries_history.sort(key=lambda daily_case: self.sort_daily_cases(daily_case, status_agg), reverse=True)
+        countrires = map(lambda country_daily_case: country_daily_case.country_id,
+                         countries_history[:data['limit']])
+
+        countrySerializer = CountrySerializer(countrires, many=True)
         return Response(countrySerializer.data)
 
+    def get_latest_country_daily_cases(self, country):
+        try:
+            return country.country_daily_cases.latest('created_at')
+        except CountryDailyCases.DoesNotExist:
+            return None
 
-class SubscribtionViewSet(ViewSet):
-    def create(self,request,*args,**kwargs):
-        serializer = SubscribtionSerializer(data={**request.data,"userId":request.user.id})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'message':'sucess','error':False,'code':201},status=status.HTTP_201_CREATED)
+    def sort_daily_cases(self, daily_case, status_agg):
+        if daily_case:
+            return getattr(daily_case, status_agg)
+        return 0
 
 
+class SubscribtionViewSet(CreateAPIView):
+    serializer_class = SubscribtionSerializer
+    queryset = Country.subscription.through.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        if 'country' not in request.data:
+            return Response({'message': 'Country is required', 'error': False, 'code': 400},
+                            status=status.HTTP_400_BAD_REQUEST)
+        country = Country.objects.filter(name=request.data['country']).first()
+        if not country:
+            return Response({'message': 'Country not found', 'error': False, 'code': 400},
+                            status=status.HTTP_400_BAD_REQUEST)
+        request.data['country'] = country.id
+        request.data['user'] = request.user.id
+        return super().post(request, *args, **kwargs)
